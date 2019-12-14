306c7cf565976b608460b810e0edb92d179e57b3
venetrius <gergelygjuhasz@gmail.com>
2019-12-14 16:27:53 -0700

    refoctor

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -8,6 +8,21 @@ const passport = require('passport');
 router.get('/github',
   passport.authenticate('github', { scope: [ 'user:email' ] }, () => console.log('call auth', arguments)));
 
+  app.get('//github/callback', function(req, res, next) {
+    passport.authenticate('github', function(err, user, info) {
+      console.log({err})
+      console.log({user})
+      if (err) { return next(err); }
+      if (!user) { return res.redirect('/login'); }
+      req.logIn(user, function(err) {
+        if (err) { return next(err); }
+        return res.redirect('/users/' + user.username);
+      });
+    })(req, res, next);
+
+  });
+
+  /*
 router.get('/github/callback', function(req, res){
   passport.authenticate(
     'github',
@@ -21,5 +36,6 @@ router.get('/github/callback', function(req, res){
     res.redirect('http://localhost:3000/?token=whatever');
   }
 });
+*/
 
 module.exports = router;
